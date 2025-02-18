24f5d501495f12e4ebd8b691b5ad95b26c24ff26
nehru00123@gmail.com <nehru00123@gmail.com>
2025-02-18 06:51:31 +0530

    angular basic setup is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'harekrishnaintl';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,63 +1,12 @@
 import { Component } from '@angular/core';
-import { DataService } from '../../services/data.service';
-import { YearlyEvent } from '../../Model/common.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+    styleUrl: './home.component.scss',
+    imports: [RouterModule]
 })
 export class HomeComponent {
-  images: string[] = [
-    'lordkrisna.png', 
-    'lordchaitanya.png', 
-    'srilaprabhupada.png'
-  ];
-  events = [
-    { date: new Date(2024, 9, 6), name: 'Angular Conference' },
-    { date: new Date(2024, 9, 7), name: 'JavaScript Meetup' },
-    { date: new Date(2024, 9, 8), name: 'RxJS Workshop' },
-    { date: new Date(2024, 9, 9), name: 'Web Development Hackathon' },
-  ];
-  currentImageIndex = 0;
-  intervalId: any;
-  selectedDate: Date;
-  monthNames = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
-  currentMonthObj: YearlyEvent;
-  currentMonth: string = '';
-
-  constructor(private dataService: DataService) {
-    const now = new Date();
-    this.currentMonth = this.monthNames[now.getMonth()];
-    this.currentMonthObj = this.dataService.yearlyEvents.find((obj) => obj.month === this.currentMonth) || { month: '', events: [] };
-    this.selectedDate = new Date(); // Default to today's date
-  }
-  ngOnInit(): void {
-      this.startImageRotation();
-      console.log(' this.currentMonthObj :',  this.currentMonthObj);
-  }
-
-  startImageRotation() {
-    this.intervalId = setInterval(() => {
-      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-    }, 5000); // Change image every 5 seconds
-  }
-  public seenDates: Set<string> = new Set();
-
-  shouldShowDate(eventDate: string): boolean {
-    if (this.seenDates.has(eventDate)) {
-      return false;
-    }
-    this.seenDates.add(eventDate);
-    return true;
-  }
   
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
 }
